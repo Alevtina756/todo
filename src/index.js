57162839eb37e6795f5ecb84860220d1d41239a2
Alevtina756 <92861415+Alevtina756@users.noreply.github.com>
2023-11-17 14:51:30 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function TodoItems() {
+  return <span> Drink Tea </span>;
+}
+//hdsjhfdhfuhduifhuidhfui
+ReactDOM.render(<TodoItems />, document.getElementById("root"));
